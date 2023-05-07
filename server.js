782044b7ceb187634cc6503e7842e1227d74c7ff
Umer Khokhar <creativeinovators@gmail.com>
2023-05-07 16:15:07 +0500

    Initial commits

--- server.js
+++ server.js
@@ -11,11 +11,23 @@ button1.addEventListener("click", () => {
   view1.style.display = "flex";
   view2.style.display = "none";
 });
-let classes = document.getElementsByClassName('view')
-console.log(classes)
-let divs = document.querySelectorAll("div")
-let setDivs = document.getElementsByTagName("div")
-console.log(setDivs)
-console.log(divs) 
+let classes = document.getElementsByClassName("view");
+console.log(classes);
+let divs = document.querySelectorAll("div");
+let setDivs = document.getElementsByTagName("div");
+console.log(setDivs);
+console.log(divs);
+let divEven = view1.querySelectorAll("div:nth-of-type(2n)");
+console.log(divEven);
 
-// QuerySelecotor and QuerySelectorAll log nodelist to the console, however the other methods like getElementById, or getElementByClassName log html element to the console!
+for (i = 0; i < divEven.length; i++) {
+  divEven[i].style.backgroundColor = "green";
+}
+
+// Selecting and Changing the navbar tiltle
+const button3 = document.getElementById("btn3");
+button3.addEventListener("click", () => {
+  let navtitle = document.querySelector("#nav b");
+  navtitle.textContent = "Hello My name is Umer";
+});
+// QuerySelecotor and QuerySelectorAll log nodelist to the console, however the other methods like getElementById, or getElementByClassName log html (Collection) elements to the console!
